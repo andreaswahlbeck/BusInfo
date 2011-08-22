80359095a8bcbea4f824c74525c9c927558dda5d
Andreas Svanberg <andreas.svanberg@dewire.com>
2011-08-22 15:09:02 +0200

    working sliding menu

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,38 +1,62 @@
+function toggleMenu() {
+	console.log('toogling menu...');
+	$('ul.menu_body').slideToggle('medium');
+}
 
+function createButton() {
+	$('button#linesButton').button({icons: {primary: "ui-icon-triangle-1-s"} });
+	console.log('applied button');
+	$('button#linesButton').click(toggleMenu);
+//	$('button#linesButton').bind({
+//		'click':toggleMenu,
+//		'mouseenter':function(){
+//			console.log('mouseenter...');
+//		},
+//	});
+	console.log('attached click...');
+}
 
+function pimpMenu() {
+	$('ul.menu_body li:even').addClass('alt');
+	
+//	$('ul.menu_body li a').mouseover(function () { 
+//		$(this).animate({ paddingLeft: "20px" }, 50 ); 
+//	});
+//
+//	$('ul.menu_body li a').mouseout(function () { 
+//		$(this).animate({ paddingLeft: "10px" }, 50 ); 
+//	});
+}
 
-
-//var mapHandler = function(){
-//	var sundsvallLatLong =  new google.maps.LatLng(62.390836, 17.306916);
-//	
-//	var mapOptions = { 
-//		    zoom: 11, 
-//		    center: sundsvallLatLong, 
-//		    mapTypeId: google.maps.MapTypeId.ROADMAP 
-//		};
-//	
-//   	resultMap = new google.maps.Map(document.getElementById(mapDiv), mapOptions);
-//	
-//	
-//}();
+function menuItemClicked(event) {
+	console.log(event.data.busLine.name + ' clicked ' + event.data.busLine.lineNumber);
+	toggleMenu();
+	$('#currentLine').html("Current line: " + event.data.busLine.name).show();
+}
 
 function getLines() {
 	console.log("getting bus lines");
 	
 	$.getJSON('service/busservice', function(data) {
-		$.each(data, function() {
-			console.log(this.name + ':' + this.lineNumber);
+		$.each(data, function(index, item) {
+			var busLine = item;
+			var menuItem = '<li><a href="#">' + busLine.name + '</a></li>';
+
+			$(menuItem).bind('click', {"busLine":busLine}, menuItemClicked).appendTo('ul.menu_body');
+			
+			if (index == data.length - 1) {
+				pimpMenu();
+			}
 		})
 	});
-	
 }
 
-
+function fillMenu() {
+	getLines();
+}
 
 $(function() {
-	
-	console.log("running gui setup");
-	
-		$("#linesButton").button().bind('click',getLines);
-		
-	});
+	createButton();
+	fillMenu();
+});
+
