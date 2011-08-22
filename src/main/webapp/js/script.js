2372e67fc604f81cc175ee39792ea96aa9336622
Andreas Svanberg <andreas.svanberg@dewire.com>
2011-08-22 15:20:29 +0200

    pimped menu click event on menu item

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,3 +1,4 @@
+
 function toggleMenu() {
 	console.log('toogling menu...');
 	$('ul.menu_body').slideToggle('medium');
@@ -5,31 +6,24 @@ function toggleMenu() {
 
 function createButton() {
 	$('button#linesButton').button({icons: {primary: "ui-icon-triangle-1-s"} });
-	console.log('applied button');
 	$('button#linesButton').click(toggleMenu);
-//	$('button#linesButton').bind({
-//		'click':toggleMenu,
-//		'mouseenter':function(){
-//			console.log('mouseenter...');
-//		},
-//	});
-	console.log('attached click...');
 }
 
 function pimpMenu() {
 	$('ul.menu_body li:even').addClass('alt');
 	
-//	$('ul.menu_body li a').mouseover(function () { 
-//		$(this).animate({ paddingLeft: "20px" }, 50 ); 
-//	});
-//
-//	$('ul.menu_body li a').mouseout(function () { 
-//		$(this).animate({ paddingLeft: "10px" }, 50 ); 
-//	});
+	$('ul.menu_body li a').mouseover(function () { 
+		$(this).animate({ paddingLeft: "20px" }, 50 ); 
+	});
+
+	$('ul.menu_body li a').mouseout(function () { 
+		$(this).animate({ paddingLeft: "10px" }, 50 ); 
+	});
 }
 
 function menuItemClicked(event) {
 	console.log(event.data.busLine.name + ' clicked ' + event.data.busLine.lineNumber);
+	getRoute(event.data.busLine.lineNumber);
 	toggleMenu();
 	$('#currentLine').html("Current line: " + event.data.busLine.name).show();
 }
@@ -48,6 +42,12 @@ function getLines() {
 				pimpMenu();
 			}
 		})
+	});
+}
+
+function getRoute(lineNumber) {
+	$.getJSON('service/busservice/route/' + lineNumber, function(data) {
+		console.log(data);
 	});
 }
 
